66d7fa1fa75eb5266f3d3832f48151bd8e874f21
Leonardo da Silva <pereira.leonardo31@escola.pr.gov.br>
2025-10-24 09:03:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,61 @@
-const avanca=document.querySelectorAll('.btn-proximo');
+startTime = Date.now();
+const vh = window.innerHeight;
 
-avanca.forEach(button=>{
-    button.addEventListener('click',function(){
-        const atual=document.querySelector('.ativo')
-        const proximoPasso='passo-'+this.getAttribute('data-proximo')
 
-        atual.classList.remove('ativo')
-        document.getElementById(proximoPasso).classList.add('ativo')
+
+
+
+
+
+
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
-
 })
 
 
 
-startTime = Date.now();
+
+
+
+
+
 function updateBodyHeight() {
-    currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-    sinusoid = Math.sin(elapsedTime/1000)*20+800;
+    const currentTime = Date.now();
+    const elapsedTime = currentTime - startTime;
+    const sinusoid = Math.sin(elapsedTime / 2000) * 20 + (vh+40);
     document.body.style.height = sinusoid + 'px';
-    console.log(seconds);
 }
-updateBodyHeight();
-setInterval(updateBodyHeight);
+
+function animate() {
+    updateBodyHeight();
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+function createRain() {
+    const rainContainer = document.createElement('div');
+    rainContainer.classList.add('rain-container');
+    document.body.appendChild(rainContainer);
+
+    for (let i = 0; i < 200; i++) {
+        const raindrop = document.createElement('div');
+        raindrop.classList.add('raindrop');
+        raindrop.style.left = Math.random() * 100 + 'vw';
+        raindrop.style.animationDelay = Math.random() * 2 + 's';
+        raindrop.style.animationDuration = (Math.random() * 1 + 1.5) + 's';
+        rainContainer.appendChild(raindrop);
+    }
+}
+
+createRain();
 
 
